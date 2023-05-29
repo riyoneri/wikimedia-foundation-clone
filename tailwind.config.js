d83b91bed234322a0d4ee8f254f78de99eeb7282
riyoneri <lionkaneza@gmail.com>
2023-05-29 09:37:22 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "dark-image": "url(/assets/bg-animation.svg)"
+      },
+      animation: {
+        'bg-slide': "slide 300s linear infinite"
+      },
+      keyframes: {
+        "slide" : {
+          '0%': {transform: 'translateX(0)'},
+          '50%': {transform: 'translateX(calc(-4237px + 100vw))'},
+          '100%': {transform: 'translateX(0)'}
+        }
+      }
+    },
+  },
+  plugins: [],
+}
