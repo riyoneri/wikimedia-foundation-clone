c92a3563a625b4fbc22ab3742b945474745d50ec
riyoneri <lionkaneza@gmail.com>
2023-05-29 17:38:26 +0200

    Add donate now button

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,25 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "dark-image": "url(/assets/bg-animation.svg)"
+        "dark-image": "url(/assets/bg-animation.svg)",
+      },
+      backgroundColor: {
+        link: "#3a25fa",
+      },
+      colors: {
+        link: "#3a25fa",
       },
       animation: {
-        'bg-slide': "slide 300s linear infinite"
+        "bg-slide": "slide 300s linear infinite",
       },
       keyframes: {
-        "slide" : {
-          '0%': {transform: 'translateX(0)'},
-          '50%': {transform: 'translateX(calc(-4237px + 100vw))'},
-          '100%': {transform: 'translateX(0)'}
-        }
-      }
+        slide: {
+          "0%": { transform: "translateX(0)" },
+          "50%": { transform: "translateX(calc(-4237px + 100vw))" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
     },
   },
   plugins: [],
-}
+};
